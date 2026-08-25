a98779fc062a2c13729a5e784146adc62fdba72b
Tarik Skalić <228550385+tarikskalic33@users.noreply.github.com>
2026-08-25 02:50:29 +0200

    fix(weil): normalize pole denominator witnesses for field

--- sovereign-omega-v2/formal/theories/Weil/FiniteEntryDictionary.v
+++ sovereign-omega-v2/formal/theories/Weil/FiniteEntryDictionary.v
@@ -78,6 +78,7 @@ Proof.
   intros a L k m n Hm Hn.
   unfold pole_closed_template, pole_kernel,
          pole_c_template, pole_s_template, pole_denominator.
+  unfold pole_denominator in Hm, Hn.
   field; try ring; try assumption.
 Qed.
 
