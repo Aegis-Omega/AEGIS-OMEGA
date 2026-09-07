ff2de7951e1bc5f3e4d99953cd7dbaacfac7e823
Tarik Skalić <228550385+tarikskalic33@users.noreply.github.com>
2026-09-07 20:45:58 +0200

    test(formal): isolate meet proof after repairing notation collision

--- verifiable/authority_meet/Authority_Meet.v
+++ verifiable/authority_meet/Authority_Meet.v
@@ -15,16 +15,16 @@ Section AuthorityPoset.
   Variable top : A.
 
   Infix "<=" := le.
-  Infix "/\" := meet (at level 40, left associativity).
+  Infix "⊓" := meet (at level 40, left associativity).
 
   (* Aksijomi Donje Polumreže (Lower Semi-Lattice axioms) *)
   Hypothesis le_refl : forall x, x <= x.
   Hypothesis le_trans : forall x y z, x <= y -> y <= z -> x <= z.
   Hypothesis le_antisym : forall x y, x <= y -> y <= x -> x = y.
 
-  Hypothesis meet_lb1 : forall x y, x /\ y <= x.
-  Hypothesis meet_lb2 : forall x y, x /\ y <= y.
-  Hypothesis meet_glb : forall x y z, z <= x -> z <= y -> z <= (x /\ y).
+  Hypothesis meet_lb1 : forall x y, x ⊓ y <= x.
+  Hypothesis meet_lb2 : forall x y, x ⊓ y <= y.
+  Hypothesis meet_glb : forall x y z, z <= x -> z <= y -> z <= (x ⊓ y).
 
   Hypothesis top_max : forall x, x <= top.
 
@@ -32,7 +32,7 @@ Section AuthorityPoset.
   Fixpoint fold_meet (l : list A) : A :=
     match l with
     | [] => top
-    | h :: t => h /\ (fold_meet t)
+    | h :: t => h ⊓ (fold_meet t)
     end.
 
   (* LEMA 1: Presjek liste je uvek manji ili jednaka bilo kom pojedinačnom elementu u listi *)
@@ -49,9 +49,9 @@ Section AuthorityPoset.
   Qed.
 
   (* Definisanje minimalne karike autoriteta čvora:
-     a_s(v) = g_s(v) /\ q_s(v) /\ fold_meet(obavezne_ivice) *)
+     a_s(v) = g_s(v) ⊓ q_s(v) ⊓ fold_meet(obavezne_ivice) *)
   Definition compute_node_authority (g_v q_v : A) (edge_authorities : list A) : A :=
-    g_v /\ q_v /\ (fold_meet edge_authorities).
+    g_v ⊓ q_v ⊓ (fold_meet edge_authorities).
 
   (* TEOREM 1: Bilo koja izvedena vrijednost autoriteta čvora ne može nadmašiti
      autoritet bilo koje obavezne ulazne ivice (Dokaz Neeskalacije) *)
@@ -61,9 +61,9 @@ Section AuthorityPoset.
   Proof.
     intros g_v q_v e_i edges Hin.
     unfold compute_node_authority.
-    (* Step 1: (g_v /\ q_v /\ fold_meet) <= (fold_meet) *)
+    (* Step 1: (g_v ⊓ q_v ⊓ fold_meet) <= (fold_meet) *)
     apply le_trans with (y := fold_meet edges).
-    - apply le_trans with (y := q_v /\ fold_meet edges).
+    - apply le_trans with (y := q_v ⊓ fold_meet edges).
       + apply meet_lb2.
       + apply meet_lb2.
     - (* Step 2: fold_meet edges <= e_i *)
@@ -74,8 +74,8 @@ Section AuthorityPoset.
      rezultujući autoritet čvora se neizbježno spušta na taj nivo *)
   Variable bot : A.
   Hypothesis bot_min : forall x, bot <= x.
-  Hypothesis meet_bot_l : forall x, bot /\ x = bot.
-  Hypothesis meet_bot_r : forall x, x /\ bot = bot.
+  Hypothesis meet_bot_l : forall x, bot ⊓ x = bot.
+  Hypothesis meet_bot_r : forall x, x ⊓ bot = bot.
 
   Theorem fail_closed_propagation : forall (g_v q_v : A) (edges : list A),
     In bot edges ->
